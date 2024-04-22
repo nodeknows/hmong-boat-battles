2ba553c372f5494357dc96c5f489bb05a4971ffb
Asian Himothy <143831802+nodeknows@users.noreply.github.com>
2024-04-21 19:14:32 -0700

    .

--- scripts/index.js
+++ scripts/index.js
@@ -87,19 +87,19 @@ const chooseCharSec = document.getElementById('chooseCharacter');
 const shaman = document.querySelector('#shaman');
 const riceFarmer = document.querySelector('#riceFarmer');
 
-shaman.onclick = function() { selectCharacter('shaman') }
-riceFarmer.onclick = function() { selectCharacter('riceFarmer') }
+shaman.onclick = function () { selectCharacter('shaman') }
+riceFarmer.onclick = function () { selectCharacter('riceFarmer') }
 
 function selectCharacter(char) {
     playerCharacters[playerSelectingChar] = char;
     if (playerSelectingChar === 2) {
         transitionToGame();
-        consoleOutput(`P2 CHOSE "${playerCharacters[2].toUpperCase()}." CHOOSE YOUR BOATS!`)
+        consoleOutput(`P1 TURN: CHOOSE YOUR BOATS!`)
     } else {
         playerSelectingChar = 2;
         outputChar(`P1 CHOSE "${playerCharacters[1].toUpperCase()}." P2 CHOOSE YOUR CHARACTER`)
     }
-    
+
 }
 
 async function transitionToGame() {
@@ -114,14 +114,14 @@ async function transitionToGame() {
 
     h3.innerHTML = '';
 
-    for (let i = 0; i <=1; i++) {
+    for (let i = 0; i <= 1; i++) {
         imgs[i].style.opacity = 0;
         h2[i].style.opacity = 0;
         ul[i].style.opacity = 0;
     }
-    
+
     await delay(1000)
-    
+
     imgs[0].remove()
     imgs[1].remove()
 
@@ -130,7 +130,7 @@ async function transitionToGame() {
 
     ul[0].remove()
     ul[1].remove()
-    
+
     frame.style.width = '0px'
     frame.style.height = '50px'
 
@@ -138,11 +138,11 @@ async function transitionToGame() {
 
     shaman.remove();
     riceFarmer.remove()
-    
+
     frame.style.backgroundColor = 'white'
     frame.style.maxHeight = 'none'
     frame.style.maxWidth = 'none'
-    
+
     frame.style.width = '100vw'
 
     await delay(1000)
@@ -150,7 +150,7 @@ async function transitionToGame() {
     frame.style.height = '100vh'
 
     await delay(1000)
-    
+
     game.style.visibility = "visible"
     frame.style.height = '5vh'
 
@@ -184,7 +184,7 @@ const rowsIndex = {
     "H": null,
 };
 
-const sqIndex = {} // sqIndex.A5, sqIndex.H7 -> Element Node
+const sqIndex = {} // sqIndex.A5, sqIndex.H7 -> Element
 
 for (let r in rowsIndex) { // Create the remaining required alphabetical rows, B-H
     if (!document.querySelector(`#${r}`)) {
@@ -222,14 +222,12 @@ function consoleOutput(msg) {
 
 // Game Handler
 let playerIdTurn = 1; // 1 = player 1, 2 = player 2
-let gameStage = 1; // 1 = selecting boats, 2 = destroying boats, 3 = victory screen
-
-consoleOutput('P1 TURN: 1x Fishing (4 Space), 2x Bamboo (3 Space), 3x Basket (1 Space) Boats Left');
 
 let boatSelected = null; // == & === null is true
-let sqsSelected = []
 
 function checkSqs(sqs) { // check if they are aligned and by how much
+    sqs.sort()
+
     let Cols = [];
     let Rows = [];
 
@@ -285,6 +283,9 @@ let maxBoatSq = 0;
 function selectBoat(boatType) {
     const boatElem = document.getElementById(boatType);
 
+    // If there is no more boats available
+    if (getRemainingBoats(boatType) === 0) return;
+
     // if there was a previously selected boat, deselect it.
     if (selectedBoat) {
         const oldBoatElem = document.getElementById(selectedBoat);
@@ -314,15 +315,244 @@ function selectBoat(boatType) {
             break;
     }
 
-    initSqSelect();
+    const plural = maxBoatSq > 1 ? 'SQUARES' : 'SQUARE';
+    consoleOutput(`P${playerIdTurn} TURN: YOU HAVE SELECTED ${boatType} BOAT. (TAKES UP ${maxBoatSq} ${plural})`);
+
+    resetSqSelect();
 }
 
 basket.onclick = function () { selectBoat('basket') };
 bamboo.onclick = function () { selectBoat('bamboo') };
 fishing.onclick = function () { selectBoat('fishing') };
 
+const boatIndex = {
+    'basket': basket,
+    'bamboo': bamboo,
+    'fishing': fishing
+}
+
 // Square Selection
+let sqsSelected = []
+let unchoosableSqs = []
+let findSq = {}; 
+// findSq.top,       findSq.left,    findSq.right,       findSq.bottom, 
+// findSq.topRight, findSq.topLeft, findSq.bottomLeft, findSq.bottomRight
+
+// BOATS ALLOWED PER PLAYER: basket (3), bamboo (2), fishing (1)
+
+let boatPositions = {
+    1: { // player
+        'basket': [], // sqsList
+        'bamboo': [],
+        'fishing': [],
+    },
+
+    2: {
+        'basket': [],
+        'bamboo': [],
+        'fishing': [],
+    }
+}
+
+for (let alpha of alphabet) {
+    for (let sq of rowsIndex[alpha].children) {
+        sq.onclick = function () {
+            if (selectedBoat) sqSelect(sq.id);
+        }
+    }
+}
+
+function sqSelect(sq) {
+    if ((selectedBoat == null) || (unchoosableSqs.includes(sq))) return;
+
+    const isAlrSelected = sqAlrSelected(sq)
+    if (isAlrSelected !== false) {
+        sqsSelected.splice(isAlrSelected, 1)
+        sqIndex[sq].style.backgroundColor = 'white'
+        return;
+    }
+
+    if (sqsSelected.length >= maxBoatSq) return;
+
+
+    sqsSelected.push(sq)
+
+    sqIndex[sq].style.backgroundColor = 'red'
+
+    if ((sqsSelected.length == maxBoatSq) && (checkSqs(sqsSelected))) { // ONCE THEY HAVE THEIR SQUARES CORRECTLY SELECTED
+        console.log('SELECTED: ' + sqsSelected)
+
+        let boatDirectory = boatPositions[playerIdTurn][selectedBoat];
+        let maxAllowedBoat = 0;
+
+        switch (selectedBoat) {
+            case 'basket':
+                maxAllowedBoat = 3;
+                break;
+            case 'bamboo':
+                maxAllowedBoat = 2;
+                break;
+            case 'fishing':
+                maxAllowedBoat = 1;
+                break;
+        };
+
+        if (boatDirectory.length < 1) {
+            boatDirectory.push([...sqsSelected]); // STUCK ON THIS PROBLEM FOR 2 DAYS! SHOUTOUT TO @ https://stackoverflow.com/questions/35737119/changing-single-object-in-js-array-changes-all-elements
+        } else if ((boatDirectory.length < 2) && (maxAllowedBoat > 1)) {
+            boatDirectory.push([...sqsSelected]);
+        } else if ((boatDirectory.length < 3) && (maxAllowedBoat > 2)) {
+            boatDirectory.push([...sqsSelected]);
+        }
+
+        resetSqSelect(boatDirectory)
+    }
+}
+
+// // if there was a previously selected boat, deselect it.
+// if (selectedBoat) {
+//     const oldBoatElem = document.getElementById(selectedBoat);
+//     oldBoatElem.style.border = 'none'
+// }
+
+// // if boat clicked is already selected, deselect it.
+// if (selectedBoat === boatType) {
+//     selectedBoat = null;
+//     boatElem.style.border = 'none';
+//     return;
+// }
+
+function sqAlrSelected(sq) {
+    const found = sqsSelected.find((rSq) => rSq === sq)
+    if (found) return sqsSelected.indexOf(found);
+    return false;
+}
+
+function resetSqSelect(boatDirectory) {
+    for (sq of sqsSelected) {
+        sq = sqIndex[sq].style.backgroundColor = 'white';
+    }
+    
+    sqsSelected = [];
+
+    if (boatDirectory) {
+        for (let sqsList of boatDirectory) {
+            for (let sq of sqsList) {
+                sqIndex[sq].style.background = 'green'
+
+                unchoosableSqs.push(findSq.top(sq))
+                unchoosableSqs.push(findSq.topRight(sq))
+                unchoosableSqs.push(findSq.right(sq))
+                unchoosableSqs.push(findSq.bottomRight(sq))
+                unchoosableSqs.push(findSq.bottom(sq))
+                unchoosableSqs.push(findSq.bottomLeft(sq))
+                unchoosableSqs.push(findSq.left(sq))
+                unchoosableSqs.push(findSq.topLeft(sq))
+            }
+        }
+
+        let maxBoats = getMaxBoats(selectedBoat);
+
+        boatIndex[selectedBoat].style.border = 'none'
+        consoleOutput(`P${playerIdTurn} TURN: YOU HAVE PLACED ${selectedBoat} (${boatDirectory.length}/${maxBoats}) BOAT. CHOOSE YOUR NEXT BOAT!`);
+        selectedBoat = null;
+    }   
+
+    // When player is done selecting ALL boats
+    if ((getRemainingBoats('basket') === 0) && (getRemainingBoats('bamboo') === 0) && (getRemainingBoats('fishing') === 0)) {
+        unchoosableSqs = [];
+        clearBoatSelection()
+
+        if (playerIdTurn === 1) {
+            playerIdTurn = 2;
+            consoleOutput(`P${playerIdTurn} TURN: YOU'RE NEXT! CHOOSE YOUR BOATS.`)
+        } else {
+
+        }
+     }
+}
+
+function clearBoatSelection() {
+    for (const [key, value] of Object.entries(sqIndex)) { // [key, value] of Object.entries(list)
+        value.style.background = 'white';
+    };
+}
+
+function getMaxBoats(boatType) {
+    let maxAllowedBoat;
+    switch (boatType) {
+        case 'basket':
+            maxAllowedBoat = 3;
+            break;
+        case 'bamboo':
+            maxAllowedBoat = 2;
+            break;
+        case 'fishing':
+            maxAllowedBoat = 1;
+            break;
+    };
+    return maxAllowedBoat;
+}
+
+function getRemainingBoats(boatType) {
+    let maxAllowedBoat = getMaxBoats(boatType);
+    return boatPositions[playerIdTurn][boatType].length-maxAllowedBoat;
+}
+
+// findSq.top,       findSq.left,    findSq.right,       findSq.bottom, 
+// findSq.topRight, findSq.topLeft, findSq.bottomLeft, findSq.bottomRight
+
+// const alphabet
+
+function alphaFind(letter, offset) { // returns the letter based off offset. (e.g letter = 'B', offset = 1, returns C)
+    const nextAlpha = alphabet[alphabet.indexOf(letter)+offset]
+    if (!nextAlpha) return letter
+    return nextAlpha;
+}
+
+function numberFind(number, offset) {
+    const nextNumber = parseInt(number)+offset;
+    if ((nextNumber > 9) || (nextNumber < 1)) return number;
+    return nextNumber;
+}
+
+findSq.top = function (sq) {
+    return alphaFind(sq[0], -1) + sq[1]
+}
+
+findSq.bottom = function (sq) {
+    return alphaFind(sq[0], 1) + sq[1]
+}
+
+findSq.left = function (sq) {
+    return sq[0] + numberFind(sq[1], -1)
+}
+
+findSq.right = function (sq) {
+    return sq[0] + numberFind(sq[1], 1)
+}
+
+//
+
+findSq.topRight = function (sq) {
+    return alphaFind(sq[0], -1) + numberFind(sq[1], 1)
+}
+
+findSq.topLeft = function (sq) {
+    return alphaFind(sq[0], -1) + numberFind(sq[1], -1)
+}
+
+findSq.bottomLeft = function (sq) {
+    return alphaFind(sq[0], 1) + numberFind(sq[1], -1)
+}
+
+findSq.bottomRight = function (sq) {
+    return alphaFind(sq[0], 1) + numberFind(sq[1], 1)
+}
 
 // FOR-LOOPS ARRAYS NOTE:
 // in | returns index
 // of | returns item
+
+// FOR-LOOPS LIST NOTE:
+// [key, value] of Object.entries(list)
